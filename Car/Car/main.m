25c5a61ff45a34c4fd69bbe47a5aa6e46d82d896
xiao yue <xiaoyue@xiaos-MacBook-Pro.local>
2019-06-03 10:31:44 -0400

    动态绑定实现方法调用

--- Car/Car/main.m
+++ Car/Car/main.m
@@ -33,7 +33,55 @@ int main(int argc, const char * argv[]) {
     [bmw run];
     
     
-
+    //处理动态方法调用NSObject
+    //判断audi是不是Car类或他的成员
+    if ([audi isKindOfClass:[Car class]])
+    {
+        NSLog(@"audi is kind of car");
+    }
+    
+    if ([audi isMemberOfClass:[Car class]])
+    {
+        NSLog(@"audi is a member of car");
+    }
+    
+    if ([car isMemberOfClass:[Car class]])
+    {
+        NSLog(@"car is a member of car");
+    }
+    
+    if ([car respondsToSelector:@selector(run)])
+    {
+         NSLog(@"car reponds to run method");
+    }
+    // 动态绑定实现方法调用
+//    SEL action;
+//    action = @selector(run);
+//    [car performSelector:action];
+    
+    // 动态绑定实现方法调用
+    [car performSelector:@selector(run)];
+    
+    if ([Benz isSubclassOfClass:[car class]])
+    {
+        NSLog(@"Benz is subClass of car ");
+    }
+    
+    //判断两个对象是不是同一类型
+    if ([audi class] == [benz class])
+    {
+        NSLog(@"Benz and audi is the same class");
+    }
+    
+    //oc 中异常处理
+    @try {
+      [car class] ;
+    } @catch (NSException *exception) {
+        NSLog(@"catch exception %@ %@", [exception name],[exception reason]);
+    } @finally {
+        NSLog(@"Code that gets executed whether or not an exception is thrown");
+    }
+    
     
     return 0;
     
